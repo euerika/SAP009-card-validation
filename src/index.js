3480537e372e7aedd8135c223cd86f7fec48a424
euerika <euerika.soaresdossantos@gmail.com>
2023-01-25 21:37:06 -0300

    Alteração html e adição de JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import validator from './validator.js';
+import { ehValido } from "./validator.js";
 
-console.log(validator);
+function validar() {
+  const inputNumeroCartao = document.getElementById("numero-cartao");
+  const numeroCartao = inputNumeroCartao.value;
+  const cartaoEhValido = ehValido(numeroCartao);
+  const resultado = document.getElementById("resultado");
+
+  if (cartaoEhValido === true) {
+    resultado.innerHTML = "Cartão Válido!";
+  } else {
+    resultado.innerHTML = "Cartão Inválido";
+  }
+}
+const btn = document.getElementById("btn-validar");
+btn.addEventListener("click", validar);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,27 @@
-const validator = {
-  // ...
-};
+export function ehValido(numeroCartao) {
+  const arrayNumeroCartao = numeroCartao.split("").reverse();
+  let soma = 0;
 
-export default validator;
+  // 1°E variavel de controle; 2° lógica de loop (até for verdade); 3° incremento da variavel de controle
+  for (let i = 0; i < arrayNumeroCartao.length; i = i + 1) {
+    console.log("indice", i, arrayNumeroCartao[i]);
+
+    if (i % 2 === 0) {
+      // pega o numero que esta na posição i e * por 2 e guarda na constante dobro
+      const dobro = arrayNumeroCartao[i] * 2;
+      if (dobro > 9) {
+        soma = soma + (dobro - 9);
+      } else {
+        soma = soma + dobro;
+      }
+    } else {
+      soma + soma + arrayNumeroCartao[i];
+    }
+  }
+  // Função para Validar o Cartão
+  if (soma % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
