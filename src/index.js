577c6daebc759ce1725e261ebdf7f38c1b24691d
euerika <euerika.soaresdossantos@gmail.com>
2023-01-31 23:13:55 -0300

    Maskify

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import { ehValido } from "./validator.js";
+import { isValid, mascCard } from "./validator.js";
 
 function validar() {
   const inputNumeroCartao = document.getElementById("numero-cartao");
   const numeroCartao = inputNumeroCartao.value;
-  const cartaoEhValido = ehValido(numeroCartao);
+  const cartaoEhValido = isValid(numeroCartao);
   const resultado = document.getElementById("resultado");
-  const maskify = ehValido(numeroCartao);
- 
-  if (cartaoEhValido === true) {
-    resultado.innerHTML = "Cartão Válido!";
+  // console.log(resultado);
+  const maskify = mascCard(numeroCartao);
+  console.log("teste", maskify);
+
+  if (numeroCartao === "") {
+    resultado.innerHTML = "Digite um número";
+  } else if (cartaoEhValido === true) {
+    resultado.innerHTML = "Cartão Válido";
   } else {
     resultado.innerHTML = "Cartão Inválido";
   }
 }
-
 const btn = document.getElementById("btn-validar");
 btn.addEventListener("click", validar);
 
-function maskify(numeroCartao) {                                                    
-  return validator.maskify(numeroCartao);
+function mascCard(numeroCartao) {
+  return mascCard(numeroCartao);
 }
--- src/validator.js
+++ src/validator.js
@@ -1,41 +1,42 @@
-export function ehValido (numeroCartao) {
-  const arrayNumeroCartao = numeroCartao.split("").reverse();
-  let soma = 0; 
+const validator = {
+  isValid(numeroCartao) {
+    const arrayNumeroCartao = numeroCartao.split("").reverse();
+    let soma = 0;
 
-  // 1° É variável de controle; 2° lógica de loop (até for verdade); 3° incremento da variavel de controle
-  for (let i = 0; i < arrayNumeroCartao.length; i = i + 1) {
-    console.log("indice", i, arrayNumeroCartao[i]);
+    // 1° É variável de controle; 2° lógica de loop (até for verdade); 3° incremento da variavel de controle
+    for (let i = 0; i < arrayNumeroCartao.length; i = i + 1) {
+      // console.log("indice", i, arrayNumeroCartao[i]);
 
-    if (i % 2 === 0) {
-      // pega o numero que esta na posição i e * por 2 e guarda na constante dobro
-      const dobro = arrayNumeroCartao[i] * 2;
-      if (dobro > 9) {
-        soma = soma + (dobro - 9);
+      if (i % 2 === 0) {
+        // pega o numero que esta na posição i e * por 2 e guarda na constante dobro
+        const dobro = arrayNumeroCartao[i] * 2;
+        if (dobro > 9) {
+          soma = soma + (dobro - 9);
+        } else {
+          soma = soma + dobro;
+        }
       } else {
-        soma = soma + dobro;
+        soma + soma + arrayNumeroCartao[i];
       }
-    } else {
-      soma + soma + arrayNumeroCartao[i];
     }
-  }
-  // Função para Validar o Cartão
-  if (soma % 10 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-// Funcão para Maskify
+    // Função para Validar o Cartão
+    if (soma % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
 
-function maskify (numeroCartao) {                
-    let maskify = [];                                                   
-  for (let i = 0; i < numeroCartao.length; i++) {                           
-    if (i < numeroCartao.length - 4) {                                       
-      maskify.push("#");                                              
-    } else {
-      maskify.push(numeroCartao[i])
+  mascCard(numeroCartao) {
+    const maskify = [];
+    for (let i = 0; i < numeroCartao.length; i = i + 1) {
+      if (i < numeroCartao.length - 4) {
+        maskify.push("#");
+      } else {
+        maskify.push(numeroCartao[i]);
+      }
     }
-  }
-  return maskify.join("");
-}                                      
-    
+    const mascarado = maskify.join("");
+    return mascarado;
+  },
+};
